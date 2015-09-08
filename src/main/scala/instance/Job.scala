6a48924d047295baaa92d2bfff72d901cdef5c03
qw0te <baert.quentin@gmail.com>
2015-09-08 14:00:42 +0200

    Ajoute une méthode Job.toString

--- src/main/scala/instance/Job.scala
+++ src/main/scala/instance/Job.scala
@@ -3,4 +3,13 @@
   *
   * @author Quentin Baert
   */
-class Job(val execTime: Int, val dueTime: Int, val weight: Int)
+class Job(val execTime: Int, val dueTime: Int, val weight: Int) {
+
+  override def toString: String =
+    "---------------------\n" +
+    "Exec   : " + this.execTime + "\n" +
+    "Due    : " + this.dueTime + "\n" +
+    "Weight : " + this.weight
+    "---------------------\n"
+
+}
