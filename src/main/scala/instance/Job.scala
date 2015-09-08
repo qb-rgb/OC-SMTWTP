126f0c70ac84a3abab2568a4450f4ff61ed24cf6
qw0te <baert.quentin@gmail.com>
2015-09-08 23:28:31 +0200

    Modifie la méthode Job.toString

--- src/main/scala/instance/Job.scala
+++ src/main/scala/instance/Job.scala
@@ -6,10 +6,8 @@
 class Job(val execTime: Int, val dueTime: Int, val weight: Int) {
 
   override def toString: String =
-    "---------------------\n" +
     "Exec   : " + this.execTime + "\n" +
     "Due    : " + this.dueTime + "\n" +
     "Weight : " + this.weight
-    "---------------------\n"
 
 }
