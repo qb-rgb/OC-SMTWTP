8f85269f24818434d301ff76869548f0af098a7b
qw0te <baert.quentin@gmail.com>
2015-09-09 09:28:53 +0200

    Corrige la lecture des données

--- src/main/scala/instance/InstanceBuilder.scala
+++ src/main/scala/instance/InstanceBuilder.scala
@@ -6,6 +6,8 @@
   */
 class InstanceBuilder(val instanceLength: Int, val valuesPerLine: Int) {
 
+  val linesPerPart = this.instanceLength / this.valuesPerLine
+
   // Read a file
   private def readFile(filePath: String): String = {
     val source = scala.io.Source.fromFile(filePath)
@@ -21,8 +23,15 @@ class InstanceBuilder(val instanceLength: Int, val valuesPerLine: Int) {
   }
 
   // Convert some lines in a list of Int
-  private def processLines(lines: Array[String]): Array[Int] =
-    lines flatMap (line => ((line split "\\s+") filterNot (_.isEmpty)) map (_.toInt))
+  private def processLines(lines: Array[String]): Array[Int] = {
+    def processLine(line: String): Array[Int] =
+      if (line.trim.isEmpty)
+        Array()
+      else
+        ((line split "\\s+") filterNot (_.isEmpty)) map (_.toInt)
+
+    lines flatMap processLine
+  }
 
   // Convert a triplet of Int in an Instance object
   private def convertTriplet(triplet: (Int, Int, Int)): Job = {
@@ -33,12 +42,9 @@ class InstanceBuilder(val instanceLength: Int, val valuesPerLine: Int) {
 
   // Read one instance from a data file
   private def readInstanceFrom(instanceLines: Array[String]): Instance = {
-    val linesPerPart = this.instanceLength / this.valuesPerLine
-    val (execTimeLines, rest0) = (instanceLines take linesPerPart, instanceLines drop linesPerPart)
-    val (dueTimeLines, rest1) = (rest0 take linesPerPart, rest0 drop linesPerPart)
-    val (weightLines, rest) = (rest1 take linesPerPart, rest1 drop linesPerPart)
-
-    assert(rest.isEmpty)
+    val (execTimeLines, rest0) = (instanceLines take this.linesPerPart, instanceLines drop this.linesPerPart)
+    val (dueTimeLines, rest1) = (rest0 take this.linesPerPart, rest0 drop this.linesPerPart)
+    val weightLines = rest1 take this.linesPerPart
 
     val execTimes = this processLines execTimeLines
     val dueTimes = this processLines dueTimeLines
@@ -55,7 +61,7 @@ class InstanceBuilder(val instanceLength: Int, val valuesPerLine: Int) {
     val lines = (this readFile file) split '\n'
     val linesPerInstance = (this.instanceLength / this.valuesPerLine) * 3
 
-    val instancesLines = (lines grouped linesPerInstance).toList
+    val instancesLines = (lines grouped linesPerInstance).toList takeWhile (_.length == linesPerInstance)
 
     instancesLines map readInstanceFrom
   }
